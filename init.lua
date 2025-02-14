670c4ccf099a33c7be771dd6b385c3dfbecba8d8
Prrooo <harshmalik00011@gmail.com>
2025-02-14 09:14:33 +0530

    add remark.lua with change in harpoon

--- init.lua
+++ init.lua
@@ -3,5 +3,3 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2 
 vim.opt.softtabstop = 2            
 vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.clipboard = "unnamedplus"
--- lua/malik/init.lua
+++ lua/malik/init.lua
@@ -1,1 +1,2 @@
 require("malik.remap")
+require("malik.set")
